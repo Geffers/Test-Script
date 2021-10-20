60ec3c2d172100e3b072c1dd2650610258809b6a
Geoff Lane <xray850@gmail.com>
2021-10-20 15:35:40 +0100

    Add files via upload
    Executable from Desktop

---
+++ NewTest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Local executable uploaded
+
+clear
+echo "This will check apt update and apt upgrade"
+sleep 10s
+sudo apt update
+echo "  "
+echo "Shall now clear screen"
+sleep 5s
+clear
+sudo apt upgrade
+echo "Shall now clear screen"
+sleep 5
+clear
+echo "  "
+echo "  "
+echo "Now clear your hard drive :)"
+
